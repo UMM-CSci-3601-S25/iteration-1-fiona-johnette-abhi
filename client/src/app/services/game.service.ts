7585a432cf67ed308cc96f00af9ab8c27d63c516
Abhineswari <seela031@umn.edu>
2025-03-05 12:55:23 -0600

    Creating a database for the server side
    Co-authored-by: Fiona Hussen
    <husse412@morris.umn.edu>
    
    Co-authored-by:Johnette <nagbe009@morris.umn.edu>

--- client/src/app/services/game.service.ts
+++ client/src/app/services/game.service.ts
@@ -1,50 +1,5 @@
-// import { Injectable } from '@angular/core';
-// import { BehaviorSubject } from 'rxjs';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class GameService {
-//   private gameCodeSubject = new BehaviorSubject<string>('');
-//   private playersJoinedSubject = new BehaviorSubject<number>(0);
-//   private gameFullSubject = new BehaviorSubject<boolean>(false);
-
-//   constructor() { }
-
-//   generateGameCode(): string {
-//     const code = Math.random().toString(36).substring(2, 8).toUpperCase();
-//     this.gameCodeSubject.next(code);
-//     return code;
-//   }
-
-//   getGameCode() {
-//     return this.gameCodeSubject.asObservable();
-//   }
-
-//   updatePlayersJoined(count: number) {
-//     this.playersJoinedSubject.next(count);
-//     if (count >= 12) {
-//       this.gameFullSubject.next(true);
-//     } else {
-//       this.gameFullSubject.next(false);
-//     }
-//   }
-
-//   getPlayersJoined() {
-//     return this.playersJoinedSubject.asObservable();
-//   }
-
-//   isGameFull() {
-//     return this.gameFullSubject.asObservable();
-//   }
-
-//   startGame() {
-//     // logic for starting the game
-//   }
-// }
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -53,10 +8,8 @@ export class GameService {
   private gameCodeSubject = new BehaviorSubject<string>('');
   private playersJoinedSubject = new BehaviorSubject<number>(0);
   private gameFullSubject = new BehaviorSubject<boolean>(false);
-  private judgeSubject = new BehaviorSubject<boolean>(false);
-  private questions: string[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   generateGameCode(): string {
     const code = Math.random().toString(36).substring(2, 8).toUpperCase();
@@ -64,11 +17,11 @@ export class GameService {
     return code;
   }
 
-  getGameCode(): Observable<string> {
+  getGameCode() {
     return this.gameCodeSubject.asObservable();
   }
 
-  updatePlayersJoined(count: number): void {
+  updatePlayersJoined(count: number) {
     this.playersJoinedSubject.next(count);
     if (count >= 12) {
       this.gameFullSubject.next(true);
@@ -77,34 +30,18 @@ export class GameService {
     }
   }
 
-  getPlayersJoined(): Observable<number> {
+  getPlayersJoined() {
     return this.playersJoinedSubject.asObservable();
   }
 
-  isGameFull(): Observable<boolean> {
+  isGameFull() {
     return this.gameFullSubject.asObservable();
   }
 
-  getJudgeStatus(): Observable<boolean> {
-    return this.judgeSubject.asObservable();
-  }
-
-  setJudgeStatus(isJudge: boolean): void {
-    this.judgeSubject.next(isJudge);
-  }
-
-  loadQuestions(): void {
-    this.http.get<{ prompts: string[] }>('/assets/qprompts.json').subscribe(data => {
-      this.questions = data.prompts;
-    });
-  }
-
-  getRandomQuestion(): string {
-    const randomIndex = Math.floor(Math.random() * this.questions.length);
-    return this.questions[randomIndex];
-  }
-
-  startGame(): void {
+  startGame() {
     // logic for starting the game
   }
 }
+
+
+
